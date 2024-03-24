957281a8b3690f1dc3f6de2a32bcf1bd3a3b7802
Keith Hazelton <khazelton@gmail.com>
2024-03-24 12:50:45 -0500

    first commit for nots-1

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,8 @@ const main = () => {
 // This was just here to force a linting error for now to demonstrate/test the
 // eslint pipeline. You can uncomment this and run "yarn check-lint" to test the
 // linting.
-// if (x == y) {
-//   console.log("equal!");
+// if (x == y) }
+//  console.log("equal!");
 // }
 
 main();
